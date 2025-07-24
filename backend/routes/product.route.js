a7a904168d728fae085ee4e69e13134ae259420b
Prajan Karthik <prjnkrthk@gmail.com>
2025-07-24 14:32:31 +0530

    Edited Product Route file

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,16 @@
-import express from 'express';
-import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/product.controller.js';
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/product.controller.js";
 const router = express.Router();
 
 router.get("/", getProducts);
 router.post("/", createProduct);
 // To update you can use both PATCH and PUT method, use PATCH to update only few fields of the object, PUT for all fields of the object
 router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct)
+router.delete("/:id", deleteProduct);
 
 export default router;
